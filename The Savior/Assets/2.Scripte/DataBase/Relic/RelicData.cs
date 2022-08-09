175c304c4aac52b10fd09fd89ec07df43fc6a8ba
Byeolsalog <syrup272@gmail.com>
2022-08-09 10:09:49 +0900

    유물 데이터 수정
    유물은 프리팹에서 지정해주는 번호와
    테이블에서 가져오는 인덱스만 가지고 있고, 인스턴스로 만들어서 데이터를 가져옴

--- The Savior/Assets/2.Scripte/DataBase/Relic/RelicData.cs
+++ The Savior/Assets/2.Scripte/DataBase/Relic/RelicData.cs
@@ -9,7 +9,7 @@ public class RelicData : MonoBehaviour
     private TextAsset jsonData;
     private string json;
 
-    public int num = 0;
+    public int number = 0;
     public int idx = 0;
     public string relicName;
     public string content;
@@ -19,7 +19,9 @@ public class RelicData : MonoBehaviour
     public int attribute;
     public int effectTypeA;
     public int effectTypeB;
-    public int useCondition;
+    public enum Conditions { 일반, 초반, 중반, 후반 };
+    public Conditions useCondition = Conditions.일반;
+    public int condition;
     public int effectDataA1;
     public int effectDataA2;
     public float effectValue;
@@ -29,7 +31,7 @@ public class RelicData : MonoBehaviour
     public float effectDataB4;
     public float effectDataC1;
 
-    private void Awake()
+    public RelicData(int num)
     {
         jsonData = Resources.Load<TextAsset>("RelicData");
         json = jsonData.text;
@@ -56,7 +58,8 @@ public class RelicData : MonoBehaviour
         attribute = data[num]["Attribute"];
         effectTypeA = data[num]["Effect_Type_A"];
         effectTypeB = data[num]["Effect_Type_B"];
-        useCondition = data[num]["Use_Condition"];
+        condition = data[num]["Use_Condition"];
+        useCondition = (Conditions)condition;
         effectDataA1 = data[num]["Effect_Data_A1"];
         effectDataA2 = data[num]["Effect_Data_A2"];
         effectValue = data[num]["Effect_Value"];
@@ -65,5 +68,10 @@ public class RelicData : MonoBehaviour
         effectDataB3 = data[num]["Effect_Data_B3"];
         effectDataB4 = data[num]["Effect_Data_B4"];
         effectDataC1 = data[num]["Effect_Data_C1"];
+    }
+    private void Awake()
+    {
+        RelicData data = new RelicData(number);
+        idx = data.idx;
     }
 }
