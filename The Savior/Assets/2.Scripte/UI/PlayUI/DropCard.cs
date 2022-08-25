58df39987f2a88332e98f0ce9fe632dc9cfa04d1
Byeolsalog <syrup272@gmail.com>
2022-08-25 12:48:07 +0900

    카드 사이즈 조정

--- The Savior/Assets/2.Scripte/UI/PlayUI/DropCard.cs
+++ The Savior/Assets/2.Scripte/UI/PlayUI/DropCard.cs
@@ -20,7 +20,7 @@ public class DropCard : MonoBehaviour, IDropHandler
         if (transform.childCount == 0 && ViewCard.dragItem != null)
         {
             ViewCard.dragItem.transform.SetParent(this.transform);
-            ViewCard.dragItem.GetComponent<Image>().rectTransform.sizeDelta = new Vector2(77, 110);
+            ViewCard.dragItem.GetComponent<Image>().rectTransform.sizeDelta = new Vector2(80, 113);
             ViewCard.dragItem.transform.position = this.transform.position;
 
             GameManager.instance.cardPreset[GameManager.instance.data.preset - 1].preset[this.transform.GetSiblingIndex()] = ViewCard.dragItem.GetComponent<ViewCard>().num;
